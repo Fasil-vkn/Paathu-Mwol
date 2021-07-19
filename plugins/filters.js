ab939d67374f6ab3150f5ce5c6d718a27d2caf0d
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-19 18:37:27 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -147,7 +147,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/mention.mp3'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-    const array = ['Paathu','Mm','sed','mention']
+    const array = ['Paathu','Mm','sed','mention','paathu','welcome']
     array.map( async (a) => {
     let pattern = new RegExp(`\\b${a}\\b`, 'g');
     if(pattern.test(message.message)){
