5a8cb06491091a193f2420fea227b1c4627ad120
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-23 12:11:58 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,30 @@
+ 
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/oBLYqk7.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Fasil*
+
+*owner number wa.me/918136831431?text=Hi*
+*githublink       https://github.com/Fasil-vkn/Paathu*
+
+
+`}) 
+
+}));
