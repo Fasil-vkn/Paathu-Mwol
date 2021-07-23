e8f3b1c987403c21a23ec0d906a980fda5225ea8
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-23 16:00:44 +0530

    Update paathu1.js

--- plugins/paathu1.js
+++ plugins/paathu1.js
@@ -19,7 +19,7 @@ const Lang = Language.getString('weather');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'paathu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
+    Asena.addCommand({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 
