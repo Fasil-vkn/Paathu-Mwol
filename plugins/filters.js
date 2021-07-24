5a34ee05f4e615a3dcfcf1aa635269c552f6efd3
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-07-24 10:25:59 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -147,7 +147,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '918547134314@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/mention.mp3'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-    const array = ['Mm','sed','mention','paathu','welcome','ചോദിക്കും','poda myre','Stanly','eid mubarak','mess','umma','zatan','ambada','mathi','Welcome','Paathu','viper','Name']
+    const array = ['Mm','sed','mention','paathu','welcome','ചോദിക്കും','poda myre','Stanly','eid mubarak','mess','umma','zatan','ambada','mathi','Welcome','Paathu','viper','Name','eh','Bc','panni','Happy','good night','Hi','Ok bei','Dance','oh']
     array.map( async (a) => {
     let pattern = new RegExp(`\\b${a}\\b`, 'g');
     if(pattern.test(message.message)){
