7e14d2e0cad0cad41afb86b2cb4b7a885a36701f
fasilvkn <84661141+fasilvkn@users.noreply.github.com>
2021-08-19 16:21:56 +0530

    Update clr.js

--- plugins/clr.js
+++ plugins/clr.js
@@ -23,7 +23,7 @@ async function checkImAdmin(message, user = message.client.user.jid) {
 
 Asena.addCommand({pattern: 'clear', fromMe: true, desc: END, dontAddCommandList: true}, (async (message, match) => {
 
-    await message.sendMessage('```cleaning chat...```');
+    await message.sendMessage('```Paathu cleaning chat...```');
     await message.client.modifyChat (message.jid, ChatModification.delete);
     await message.sendMessage('```🏳 Chat cleared 🏳```');
 }));
